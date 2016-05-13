487b3f065929fbf7cd4229cd5b22c42ffaa5a847
Cameron Rex <camrex@rmimidwest.com>
2016-05-13 15:09:13 -0500

    Formatting

--- SDL_Pi_WeatherRack.py
+++ SDL_Pi_WeatherRack.py
@@ -5,63 +5,47 @@
 #
 #  SparkFun Weather Station Meters
 #  Argent Data Systems
-#  Created by SwitchDoc Labs February 13, 2015 
+#  Created by SwitchDoc Labs February 13, 2015
 #  Released into the public domain.
 #    Version 1.3 - remove 300ms Bounce
 #    Version 2.0 - Update for WeatherPiArduino V2
-#    
+#
 
 # imports
-
-try:
-	import config
-except:
-	import NoWPAConfig.py as config	
-
 import sys
 import time as time_
-
-sys.path.append('./Adafruit_ADS1x15')
-
+from datetime import *
+import RPi.GPIO as GPIO
 from Adafruit_ADS1x15 import ADS1x15
 
-import RPi.GPIO as GPIO
-
+try:
+    import config
+except:
+    import NoWPAConfig.py as config
 
 GPIO.setwarnings(False)
 
-from datetime import *
-
-
 # constants
-
 SDL_MODE_INTERNAL_AD = 0
 SDL_MODE_I2C_ADS1015 = 1
-
-#sample mode means return immediately.  THe wind speed is averaged at sampleTime or when you ask, whichever is longer
-SDL_MODE_SAMPLE = 0
-#Delay mode means to wait for sampleTime and the average after that time.
-SDL_MODE_DELAY = 1
-
-WIND_FACTOR = 2.400 
+SDL_MODE_SAMPLE = 0  # Sample mode means return immediately.  THe wind speed is averaged at sampleTime or when you ask, whichever is longer
+SDL_MODE_DELAY = 1  # Delay mode means to wait for sampleTime and the average after that time.
+WIND_FACTOR = 2.400
 
 # Helper Functions
 
 
 def fuzzyCompare(compareValue, value):
-	
-	VARYVALUE = 0.05
+    VARYVALUE = 0.05
+    if ((value > (compareValue * (1.0 - VARYVALUE))) and (value < (compareValue * (1.0 + VARYVALUE)))):
+        return True
+    return False
 
-   	if ( (value > (compareValue * (1.0-VARYVALUE)))  and (value < (compareValue *(1.0+VARYVALUE))) ):
-     		return True
-        
-	return False
- 
+
+
 def voltageToDegrees(value, defaultWindDirection):
-    	# Note:  The original documentation for the wind vane says 16 positions.  Typically only recieve 8 positions.  And 315 degrees was wrong.
-     
-    	# For 5V, use 1.0.  For 3.3V use 0.66
-	ADJUST3OR5 = 0.66
+    # Note:  The original documentation for the wind vane says 16 positions.  Typically only recieve 8 positions.  And 315 degrees was wrong.
+	ADJUST3OR5 = 0.66  # For 5V, use 1.0.  For 3.3V use 0.66
 	PowerVoltage = 3.3
 
 
@@ -73,10 +57,10 @@ def voltageToDegrees(value, defaultWindDirection):
 
 	if (fuzzyCompare(2.25 * ADJUST3OR5, value)):
 		return 45
-				  
+
 	if (fuzzyCompare(0.41 * ADJUST3OR5, value)):
 		return 67.5
-					  
+
 	if (fuzzyCompare(0.45 * ADJUST3OR5, value)):
 		return 90.0
 
@@ -103,16 +87,16 @@ def voltageToDegrees(value, defaultWindDirection):
 
 	if (fuzzyCompare(4.62 * ADJUST3OR5, value)):
 		return 270.0
-	
+
 	if (fuzzyCompare(4.04 * ADJUST3OR5, value)):
 		return 292.5
 
 	if (fuzzyCompare(4.34 * ADJUST3OR5, value)): # chart in manufacturers documentation wrong
 		return 315.0
-	
+
 	if (fuzzyCompare(3.43 * ADJUST3OR5, value)):
 		return 337.5
-	
+
 	return defaultWindDirection  # return previous value if not found
 
 # return current microseconds
@@ -150,7 +134,7 @@ class SDL_Pi_WeatherRack:
 
         _lastWindTime = 0
 	_shortestWindTime = 0
-		         
+
 	_sampleTime = 5.0
 	_selectedMode = SDL_MODE_SAMPLE
  	_startSampleTime = 0
@@ -165,12 +149,12 @@ class SDL_Pi_WeatherRack:
 
 		GPIO.setup(pinAnem, GPIO.IN)
 		GPIO.setup(pinRain, GPIO.IN)
-		
-		# when a falling edge is detected on port pinAnem, regardless of whatever   
-		# else is happening in the program, the function callback will be run  
-	
-		GPIO.add_event_detect(pinAnem, GPIO.RISING, callback=self.serviceInterruptAnem )  
-		GPIO.add_event_detect(pinRain, GPIO.RISING, callback=self.serviceInterruptRain )  
+
+		# when a falling edge is detected on port pinAnem, regardless of whatever
+		# else is happening in the program, the function callback will be run
+
+		GPIO.add_event_detect(pinAnem, GPIO.RISING, callback=self.serviceInterruptAnem )
+		GPIO.add_event_detect(pinRain, GPIO.RISING, callback=self.serviceInterruptRain )
 
 		ADS1015 = 0x00  # 12-bit ADC
 		ADS1115 = 0x01  # 16-bit ADC
@@ -227,7 +211,7 @@ class SDL_Pi_WeatherRack:
 	# Wind Direction Routines
 
 	def current_wind_direction(self):
-    
+
     		if (SDL_Pi_WeatherRack._ADMode == SDL_MODE_I2C_ADS1015):
 			value = self.ads1015.readADCSingleEnded(1, self.gain, self.sps) # AIN1 wired to wind vane on WeatherPiArduino
 
@@ -236,14 +220,14 @@ class SDL_Pi_WeatherRack:
     		else:
       			# user internal A/D converter
       			voltageValue = 0.0
-  
+
 		direction = voltageToDegrees(voltageValue, SDL_Pi_WeatherRack._currentWindDirection)
     		return direction;
 
 
 
 	def current_wind_direction_voltage(self):
-    
+
     		if (SDL_Pi_WeatherRack._ADMode == SDL_MODE_I2C_ADS1015):
 			value = self.ads1015.readADCSingleEnded(1, self.gain, self.sps) # AIN1 wired to wind vane on WeatherPiArduino
 
@@ -252,7 +236,7 @@ class SDL_Pi_WeatherRack:
     		else:
       			# user internal A/D converter
       			voltageValue = 0.0
-  
+
     		return voltageValue
 
 	# Utility methods
@@ -268,42 +252,42 @@ class SDL_Pi_WeatherRack:
 
 
 	def startWindSample(self, sampleTime):
-  
+
       		SDL_Pi_WeatherRack._startSampleTime = micros();
-     
+
       		SDL_Pi_WeatherRack._sampleTime = sampleTime;
-      
-	# get current wind 
+
+	# get current wind
 	def get_current_wind_speed_when_sampling(self):
 
 
    		compareValue = SDL_Pi_WeatherRack._sampleTime*1000000;
- 
+
    		if (micros() - SDL_Pi_WeatherRack._startSampleTime >= compareValue):
       			# sample time exceeded, calculate currentWindSpeed
       			timeSpan = (micros() - SDL_Pi_WeatherRack._startSampleTime);
- 
+
       			SDL_Pi_WeatherRack._currentWindSpeed = (float(SDL_Pi_WeatherRack._currentWindCount)/float(timeSpan)) * WIND_FACTOR*1000000.0
 
-			#print "SDL_CWS = %f, SDL_Pi_WeatherRack._shortestWindTime = %i, CWCount=%i TPS=%f" % (SDL_Pi_WeatherRack._currentWindSpeed,SDL_Pi_WeatherRack._shortestWindTime, SDL_Pi_WeatherRack._currentWindCount, float(SDL_Pi_WeatherRack._currentWindCount)/float(SDL_Pi_WeatherRack._sampleTime)) 
+			#print "SDL_CWS = %f, SDL_Pi_WeatherRack._shortestWindTime = %i, CWCount=%i TPS=%f" % (SDL_Pi_WeatherRack._currentWindSpeed,SDL_Pi_WeatherRack._shortestWindTime, SDL_Pi_WeatherRack._currentWindCount, float(SDL_Pi_WeatherRack._currentWindCount)/float(SDL_Pi_WeatherRack._sampleTime))
 
       			SDL_Pi_WeatherRack._currentWindCount = 0
-      
+
       			SDL_Pi_WeatherRack._startSampleTime = micros()
 
- 		#print "SDL_Pi_WeatherRack._currentWindSpeed=", SDL_Pi_WeatherRack._currentWindSpeed 
+ 		#print "SDL_Pi_WeatherRack._currentWindSpeed=", SDL_Pi_WeatherRack._currentWindSpeed
    		return SDL_Pi_WeatherRack._currentWindSpeed
 
 
 
-	def setWindMode(self, selectedMode, sampleTime): # time in seconds 
-  
+	def setWindMode(self, selectedMode, sampleTime): # time in seconds
+
   		SDL_Pi_WeatherRack._sampleTime = sampleTime;
   		SDL_Pi_WeatherRack._selectedMode = selectedMode;
-  
+
   		if (SDL_Pi_WeatherRack._selectedMode == SDL_MODE_SAMPLE):
   			self.startWindSample(SDL_Pi_WeatherRack._sampleTime);
-  
+
 	#def get current values
 
 	def get_current_rain_total(self):
@@ -315,12 +299,12 @@ class SDL_Pi_WeatherRack:
 
 
 	def current_wind_speed(self): # in milliseconds
-  
+
   		if (SDL_Pi_WeatherRack._selectedMode == SDL_MODE_SAMPLE):
     			SDL_Pi_WeatherRack._currentWindSpeed = self.get_current_wind_speed_when_sampling();
   		else:
     			# km/h * 1000 msec
-    
+
     			SDL_Pi_WeatherRack._currentWindCount = 0;
     			delay(SDL_Pi_WeatherRack._sampleTime*1000);
     			SDL_Pi_WeatherRack._currentWindSpeed = (float(SDL_Pi_WeatherRack._currentWindCount)/float(SDL_Pi_WeatherRack._sampleTime)) * WIND_FACTOR;
@@ -329,7 +313,7 @@ class SDL_Pi_WeatherRack:
   		return SDL_Pi_WeatherRack._currentWindSpeed;
 
 	def get_wind_gust(self):
-   
+
   		latestTime =SDL_Pi_WeatherRack._shortestWindTime;
   		SDL_Pi_WeatherRack._shortestWindTime=0xffffffff;
   		time=latestTime/1000000.0;  # in microseconds
@@ -339,7 +323,7 @@ class SDL_Pi_WeatherRack:
   			return (1.0/float(time))*WIND_FACTOR;
 
 
-  
+
 
 	# Interrupt Routines
 
@@ -357,12 +341,12 @@ class SDL_Pi_WeatherRack:
 
      			if(currentTime<SDL_Pi_WeatherRack._shortestWindTime):
      				SDL_Pi_WeatherRack._shortestWindTime=currentTime;
- 
+
 
 
 
 	def serviceInterruptRain(self,channel):
-		
+
 		#print "Rain Interrupt Service Routine"
 
   		currentTime=(micros()-SDL_Pi_WeatherRack._lastRainTime);
@@ -372,4 +356,3 @@ class SDL_Pi_WeatherRack:
        			SDL_Pi_WeatherRack._currentRainCount = SDL_Pi_WeatherRack._currentRainCount+1
     			if(currentTime<SDL_Pi_WeatherRack._currentRainMin):
      				SDL_Pi_WeatherRack._currentRainMin=currentTime;
-
